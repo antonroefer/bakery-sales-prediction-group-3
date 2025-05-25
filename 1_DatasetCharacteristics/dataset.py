7c530a8b8be438d098b8ca3f1605b877ca5d93fe
antonroefer <anton.roefer@web.de>
2025-05-25 10:38:05 +0200

    Split into train, validation and test data in dataset.py aswell as new folder "Input_Data" for better readability

--- 1_DatasetCharacteristics/dataset.py
+++ 1_DatasetCharacteristics/dataset.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Load the CSV files
-train = pd.read_csv("train.csv")
-test = pd.read_csv("test.csv")
+train = pd.read_csv("Input_Data/train.csv")
+test = pd.read_csv("Input_Data/test.csv")
 
 # Concatenate train and test DataFrames
 data = pd.concat([train, test], ignore_index=True)
@@ -25,26 +25,35 @@ data = pd.merge(data, kiwo, on="Datum", how="left")
 data["KielerWoche"] = data["KielerWoche"].fillna(0)
 
 # Load Schulferien.csv
-schulferien = pd.read_csv("Schulferien.csv")
+schulferien = pd.read_csv("Input_Data/Schulferien.csv")
 data = pd.merge(data, schulferien, on="Datum", how="left")
 
 # Load Feiertage_SH.csv
-feiertage = pd.read_csv("Feiertage_SH.csv")
+feiertage = pd.read_csv("Input_Data/Feiertage_SH.csv")
 feiertage["Datum"] = pd.to_datetime(feiertage["Datum"], dayfirst=True).dt.strftime(
     "%Y-%m-%d"
 )
 data["Feiertage"] = data["Datum"].isin(feiertage["Datum"]).astype(int)
 
 # Load wahltage_binaer.csv
-wahltage = pd.read_csv("wahltage_binaer.csv")
+wahltage = pd.read_csv("Input_Data/wahltage_binaer.csv")
 wahltage["Datum"] = pd.to_datetime(wahltage["Datum"]).dt.strftime("%Y-%m-%d")
 data = pd.merge(data, wahltage, on="Datum", how="left")
 data = data.rename(columns={"NeueSpalte": "Wahltag"})
 data["Wahltag"] = data["Wahltag"].fillna(0)
 
 # Load VPI_modified.csv and merge VPI
-vpi = pd.read_csv("VPI_modified.csv")
+vpi = pd.read_csv("Input_Data/VPI_modified.csv")
 vpi["Datum"] = pd.to_datetime(vpi["Datum"]).dt.strftime("%Y-%m-%d")
 data = pd.merge(data, vpi[["Datum", "VPI"]], on="Datum", how="left")
 
 data.to_csv("data.csv", index=False)
+
+
+training_df = data[(data["Datum"] >= "2013-07-01") & (data["Datum"] <= "2017-07-31")]
+validation_df = data[(data["Datum"] >= "2017-08-01") & (data["Datum"] <= "2018-07-31")]
+test_df = data[(data["Datum"] >= "2018-08-01") & (data["Datum"] <= "2019-07-31")]
+
+print("\nTraining set shape:", training_df.shape)
+print("Validation set shape:", validation_df.shape)
+print("Test set shape:", test_df.shape)
